6117bc0260dc6959d9bd99dd486f36f824a6caf7
Gabriel Souza <devsrsouza@gmail.com>
2022-03-27 14:31:00 -0300

    chore: removing TODO

--- voyager-navigator/src/uikitMain/kotlin/cafe.adriel.voyager.navigator.internal/Actuals.uikit.kt
+++ voyager-navigator/src/uikitMain/kotlin/cafe.adriel.voyager.navigator.internal/Actuals.uikit.kt
@@ -2,6 +2,5 @@ package cafe.adriel.voyager.navigator.internal
 
 import androidx.compose.runtime.Composable
 
-// TODO: use ios backstack
 @Composable
 internal actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) = Unit
