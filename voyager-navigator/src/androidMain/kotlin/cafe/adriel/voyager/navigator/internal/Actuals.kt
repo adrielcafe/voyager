fc43db47d7f7e4d9ab4536676f5640e21fd30ed4
Syer10 <syer10@users.noreply.github.com>
2022-10-01 11:49:11 -0400

    Cleanup imports

--- voyager-navigator/src/androidMain/kotlin/cafe/adriel/voyager/navigator/internal/Actuals.kt
+++ voyager-navigator/src/androidMain/kotlin/cafe/adriel/voyager/navigator/internal/Actuals.kt
@@ -1,13 +1,7 @@
 package cafe.adriel.voyager.navigator.internal
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 internal actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) = BackHandler(enabled, onBack)
--- voyager-navigator/src/desktopMain/kotlin/cafe/adriel/voyager/navigator/internal/Actuals.kt
+++ voyager-navigator/src/desktopMain/kotlin/cafe/adriel/voyager/navigator/internal/Actuals.kt
@@ -1,7 +1,6 @@
 package cafe.adriel.voyager.navigator.internal
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 
 @Composable
 internal actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) = Unit
