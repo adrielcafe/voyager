a6a391099667886b2070da47abf6ec3dd9c8185a
Syer10 <syer10@users.noreply.github.com>
2022-10-27 14:34:21 -0400

    Implement getDefaultViewModelCreationExtras

--- voyager-androidx/src/main/java/cafe/adriel/voyager/androidx/AndroidScreenLifecycleOwner.kt
+++ voyager-androidx/src/main/java/cafe/adriel/voyager/androidx/AndroidScreenLifecycleOwner.kt
@@ -3,6 +3,7 @@ package cafe.adriel.voyager.androidx
 import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.content.ContextWrapper
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
@@ -12,10 +13,15 @@ import androidx.lifecycle.HasDefaultViewModelProviderFactory
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.SAVED_STATE_REGISTRY_OWNER_KEY
 import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.savedstate.SavedStateRegistry
 import androidx.savedstate.SavedStateRegistryController
@@ -81,6 +87,27 @@ public class AndroidScreenLifecycleOwner private constructor() :
             (atomicContext.get()?.applicationContext as? Application),
             this
         )
+    }
+
+    override fun getDefaultViewModelCreationExtras(): CreationExtras = MutableCreationExtras().apply {
+        var application: Application? = null
+        var context = atomicContext.get()?.applicationContext
+        while (context is ContextWrapper) {
+            if (context is Application) {
+                application = context
+                break
+            }
+            context = context.baseContext
+        }
+        if (application != null) {
+            set(AndroidViewModelFactory.APPLICATION_KEY, application)
+        }
+        set(SAVED_STATE_REGISTRY_OWNER_KEY, this@AndroidScreenLifecycleOwner)
+        set(VIEW_MODEL_STORE_OWNER_KEY, this@AndroidScreenLifecycleOwner)
+
+        /* TODO if (getArguments() != null) {
+            extras.set<Bundle>(DEFAULT_ARGS_KEY, getArguments())
+        }*/
     }
 
     public companion object {
