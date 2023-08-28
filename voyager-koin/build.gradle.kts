d10e9182464b64080230bd4b4f1de61dbf01fba8
florian schoeberl <florianschoeberl@gmail.com>
2023-08-28 15:10:29 +0200

    Add compose multiplatform support to voyager-koin (#159)

--- voyager-koin/build.gradle.kts
+++ voyager-koin/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    kotlin("android")
+    kotlin("multiplatform")
     id("com.android.library")
+    id("org.jetbrains.compose")
     id("com.vanniktech.maven.publish")
 }
 
-setupModuleForAndroidxCompose()
+setupModuleForComposeMultiplatform(fullyMultiplatform = true)
+
 
 android {
     namespace = "cafe.adriel.voyager.koin"
@@ -13,12 +15,18 @@ android {
     }
 }
 
-dependencies {
-    api(projects.voyagerCore)
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(projects.voyagerCore)
 
-    implementation(libs.koin)
-    implementation(libs.compose.runtime)
+                compileOnly(compose.runtime)
+                compileOnly(libs.composeMultiplatform.runtimeSaveable)
 
-    testRuntimeOnly(libs.junit.engine)
-    testImplementation(libs.junit.api)
+                implementation(libs.coroutines)
+                implementation(libs.koin.compose)
+            }
+        }
+    }
 }
