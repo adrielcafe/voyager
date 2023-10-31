fdfe9ed65e6380e3eaf3d58d791a10be1de65f35
DevSrSouza <devsrsouza@gmail.com>
2023-10-30 21:11:21 -0300

    fix: hilt viewmodel onCleared not called

--- voyager-hilt/src/main/java/cafe/adriel/voyager/hilt/internal/ContextExt.kt
+++ voyager-hilt/src/main/java/cafe/adriel/voyager/hilt/internal/ContextExt.kt
@@ -5,9 +5,7 @@ import android.content.ContextWrapper
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
 
-// Unfortunately findOwner function is internal in activity-compose
-// TODO: Maybe move to androidx module because we'll need this function when implement onCloseRequest support
-internal inline fun <reified T> findOwner(context: Context): T? {
+private inline fun <reified T> findOwner(context: Context): T? {
     var innerContext = context
     while (innerContext is ContextWrapper) {
         if (innerContext is T) {
@@ -22,7 +20,3 @@ internal inline fun <reified T> findOwner(context: Context): T? {
 internal val Context.componentActivity: ComponentActivity
     get() = findOwner<ComponentActivity>(this)
         ?: error("Context must be a androidx.activity.ComponentActivity. Current is $this")
-
-@PublishedApi
-internal val Context.defaultViewModelProviderFactory: ViewModelProvider.Factory
-    get() = componentActivity.defaultViewModelProviderFactory
