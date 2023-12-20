ab4e3a1e24402c14d54826a3df496023371661bb
DevSrSouza <devsrsouza@gmail.com>
2023-12-20 19:12:42 -0300

    chore: migrate iOS sample to a xcode project

---
+++ samples/multiplatform/src/iosMain/kotlin/MainViewController.kt
@@ -0,0 +1,4 @@
+import androidx.compose.ui.window.ComposeUIViewController
+import cafe.adriel.voyager.sample.multiplatform.SampleApplication
+
+fun MainViewController() = ComposeUIViewController { SampleApplication() }
