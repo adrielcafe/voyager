764388a937b2fb1b0f56016004b81be18c09704f
Gabriel Souza <devsrsouza@gmail.com>
2024-05-21 08:32:02 -0300

    Per screen transition experimental api (#424)
    * Per Screen transition experimental API
    
    * Update transitionSpec to avoid behavior change
    
    * fix lint
    
    * update api dump

--- voyager-transitions/src/commonMain/kotlin/cafe/adriel/voyager/transitions/ScreenTransition.kt
+++ voyager-transitions/src/commonMain/kotlin/cafe/adriel/voyager/transitions/ScreenTransition.kt
@@ -4,11 +4,33 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.stack.StackEvent
 import cafe.adriel.voyager.navigator.Navigator
+
+@ExperimentalVoyagerApi
+public interface ScreenTransition {
+
+    /**
+     * Defines the enter transition for the Screen.
+     *
+     * Returns null when it should not define a transition for this screen.
+     */
+    public fun enter(): EnterTransition? = null
+
+    /**
+     * Defines the exit transition for the Screen.
+     *
+     * Returns null when it should not define a transition for this screen.
+     */
+    public fun exit(): ExitTransition? = null
+}
 
 public typealias ScreenTransitionContent = @Composable AnimatedVisibilityScope.(Screen) -> Unit
 
@@ -42,7 +64,30 @@ public fun ScreenTransition(
 ) {
     AnimatedContent(
         targetState = navigator.lastItem,
-        transitionSpec = transition,
+        transitionSpec = {
+            val contentTransform = transition()
+
+            val isPop = navigator.lastEvent == StackEvent.Pop
+
+            val screenEnterTransition = if (isPop) {
+                (targetState as? ScreenTransition)?.enter()
+            } else {
+                (targetState as? ScreenTransition)?.enter()
+            }
+
+            val screenExitTransition = if (isPop) {
+                (initialState as? ScreenTransition)?.exit()
+            } else {
+                (initialState as? ScreenTransition)?.exit()
+            }
+
+            if (screenExitTransition != null || screenEnterTransition != null) {
+                (screenEnterTransition ?: contentTransform.targetContentEnter) togetherWith
+                    (screenExitTransition ?: contentTransform.initialContentExit)
+            } else {
+                contentTransform
+            }
+        },
         modifier = modifier
     ) { screen ->
         navigator.saveableState("transition", screen) {
