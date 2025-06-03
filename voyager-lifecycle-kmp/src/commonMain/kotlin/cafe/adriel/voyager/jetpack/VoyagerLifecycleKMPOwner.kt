8c3141de325fd1908fe29e6550fb1437cd8357a1
Gabriel Souza <devsrsouza@gmail.com>
2025-06-03 14:33:00 -0300

    Fixing lint issues

--- voyager-lifecycle-kmp/src/commonMain/kotlin/cafe/adriel/voyager/jetpack/VoyagerLifecycleKMPOwner.kt
+++ voyager-lifecycle-kmp/src/commonMain/kotlin/cafe/adriel/voyager/jetpack/VoyagerLifecycleKMPOwner.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.core.bundle.Bundle
 import androidx.lifecycle.AtomicReference
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.HasDefaultViewModelProviderFactory
