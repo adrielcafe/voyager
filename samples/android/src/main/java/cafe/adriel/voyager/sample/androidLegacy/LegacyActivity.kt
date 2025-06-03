97bf62e9a3350f2e671f2d916f9f0010f4f8ed8f
Gabriel Souza <devsrsouza@gmail.com>
2025-06-02 21:44:43 -0300

    Fixing crash causes in older versions because of removeFirst and removeLast

--- samples/android/src/main/java/cafe/adriel/voyager/sample/androidLegacy/LegacyActivity.kt
+++ samples/android/src/main/java/cafe/adriel/voyager/sample/androidLegacy/LegacyActivity.kt
@@ -1,14 +1,14 @@
 package cafe.adriel.voyager.sample.androidLegacy
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.ComposeView
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.sample.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LegacyActivity : AppCompatActivity() {
+class LegacyActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_legacy)
---
+++ voyager-core/src/commonMain/kotlin/cafe/adriel/voyager/core/internal/SafeCollections.kt
@@ -0,0 +1,11 @@
+package cafe.adriel.voyager.core.internal
+
+/**
+ * https://youtrack.jetbrains.com/issue/KT-71375/Prevent-Kotlins-removeFirst-and-removeLast-from-causing-crashes-on-Android-14-and-below-after-upgrading-to-Android-API-Level-35
+ */
+
+internal fun <T> MutableList<T>.removeFirstElement(): T = if (isEmpty()) throw NoSuchElementException("List is empty.") else removeAt(0)
+
+internal fun <T> MutableList<T>.removeFirstElementOrNull(): T? = if (isEmpty()) null else removeAt(0)
+
+internal fun <T> MutableList<T>.removeLastElement(): T = if (isEmpty()) throw NoSuchElementException("List is empty.") else removeAt(lastIndex)
