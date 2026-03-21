8cd4d23da769ac9fe8cb03fcb371a81f9d96cb53
Thiago Santos <programadorthi@gmail.com>
2026-03-21 15:13:27 -0300

    fix: ktlint voyager-rxjava

--- voyager-rxjava/src/commonJvmMain/kotlin/cafe/adriel/voyager/rxjava/ScreenModel.kt
+++ voyager-rxjava/src/commonJvmMain/kotlin/cafe/adriel/voyager/rxjava/ScreenModel.kt
@@ -7,15 +7,15 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 public val ScreenModel.disposables: CompositeDisposable
-    get() = ScreenModelStore.getOrPutDependency(
-        screenModel = this,
-        name = "ScreenModelCompositeDisposable",
-        factory = { CompositeDisposable() },
-        onDispose = { disposables -> disposables.clear() }
-    )
+    get() =
+        ScreenModelStore.getOrPutDependency(
+            screenModel = this,
+            name = "ScreenModelCompositeDisposable",
+            factory = { CompositeDisposable() },
+            onDispose = { disposables -> disposables.clear() },
+        )
 
 public abstract class RxScreenModel<S : Any> : ScreenModel {
-
     protected val mutableState: BehaviorSubject<S> = BehaviorSubject.create()
     public val state: Observable<S> = mutableState
 }
