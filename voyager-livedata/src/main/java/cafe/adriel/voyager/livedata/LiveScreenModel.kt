39e4c9a6fdfa10488615564f0fe589a34d5730fa
Thiago Santos <programadorthi@gmail.com>
2026-03-21 15:11:07 -0300

    fix: ktlint voyager-livedata

--- voyager-livedata/src/main/java/cafe/adriel/voyager/livedata/LiveScreenModel.kt
+++ voyager-livedata/src/main/java/cafe/adriel/voyager/livedata/LiveScreenModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import cafe.adriel.voyager.core.model.ScreenModel
 
 public abstract class LiveScreenModel<S>(initialState: S) : ScreenModel {
-
     protected val mutableState: MutableLiveData<S> = MutableLiveData(initialState)
     public val state: LiveData<S> = mutableState
 }
